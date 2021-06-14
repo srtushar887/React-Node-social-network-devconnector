ac1ed42d956892dc9d70accaf1acab59c2ee9e67
srtushar887 <sr.tushar.pro@gmail.com>
2021-06-14 14:36:00 +0600

    react login register added

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React,{Fragment} from "react";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+
+//redux
+import {Provider} from "react-redux";
+import store from "./store";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import './App.css';
+
+const App =()=> (
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar/>
+                <Route exact path="/" component={Landing}></Route>
+                <section className="container">
+                    <Switch>
+                        <Route exact path="/register" component={Register}></Route>
+                        <Route exact path="/Login" component={Login}></Route>
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
+)
+
+export default App;
